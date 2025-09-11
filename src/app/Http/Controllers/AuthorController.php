25396785ca0058e5fb039ae392f36fb52f7423f9
Tetsuya Kishi <158604040+teestojko@users.noreply.github.com>
2025-09-11 17:11:33 +0900

    2-5 モデルのリレーションの基本を学ぼう

--- src/app/Http/Controllers/AuthorController.php
+++ src/app/Http/Controllers/AuthorController.php
@@ -76,4 +76,12 @@ class AuthorController extends Controller
     {
         return view('verror');
     }
+
+    public function relate()
+    {
+        $hasItems = Author::has('book')->get();
+        $noItems = Author::doesntHave('book')->get();
+        $param = ['hasItems' => $hasItems, 'noItems' => $noItems];
+        return view('author.index',$param);
+    }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,3 +29,11 @@ Route::post('/find', [AuthorController::class, 'search']);
 Route::get('/author/{author}', [AuthorController::class, 'bind']);
 
 Route::get('/verror', [AuthorController::class, 'verror']);
+
+Route::get('/relation', [AuthorController::class, 'relate']);
+
+Route::prefix('book')->group(function() {
+    Route::get('/', [BookController::class, 'index']);
+    Route::get('/add', [BookController::class, 'add']);
+    Route::post('/add', [BookController::class, 'create']);
+});
